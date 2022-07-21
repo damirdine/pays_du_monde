804773f52649b4a86005635eaac608857f2fc33d
damirdine <damirdine@outlook.fr>
2022-07-21 13:35:35 +0200

    front

--- index.php
+++ index.php
@@ -15,27 +15,27 @@ $continents = $ContinentPrep->fetchAll();
 
 // REQUETTE RECUP TOUT LES REGIONS
 $regionRequest = "SELECT * FROM t_regions";
-$regionPrep = $db -> query($regionRequest);
+$regionPrep = $db->query($regionRequest);
 $regions = $regionPrep->fetchAll();
 
-if(isset($_GET["continent"],$_GET["region"])){
-    if($_GET["region"]==""){
+if (isset($_GET["continent"], $_GET["region"])) {
+    if ($_GET["region"] == "") {
         $SqlREquest = "SELECT * 
             FROM t_pays p
             WHERE p.continent_id = " . $_GET["continent"];
-    }else{
+    } else {
         $SqlREquest = "SELECT * 
         FROM t_pays p
-        WHERE p.continent_id = " . $_GET["continent"] . " AND p.region_id = " .(int)$_GET["region"]; 
+        WHERE p.continent_id = " . $_GET["continent"] . " AND p.region_id = " . (int)$_GET["region"];
     }
-    $statsPrep = $db -> query($SqlREquest);
+    $statsPrep = $db->query($SqlREquest);
     $stats = $statsPrep->fetchAll();
 
     $continentID = $_GET["continent"];
 }
-if(isset($_GET["region"],$_GET["continent"])&& $_GET["continent"]!=""){
-    $regionRequest = "SELECT * FROM t_regions r WHERE r.continent_id =" .$_GET["continent"];
-    $regionPrep = $db -> query($regionRequest);
+if (isset($_GET["region"], $_GET["continent"]) && $_GET["continent"] != "") {
+    $regionRequest = "SELECT * FROM t_regions r WHERE r.continent_id =" . $_GET["continent"];
+    $regionPrep = $db->query($regionRequest);
     $regions = $regionPrep->fetchAll();
 }
 ?>
@@ -43,60 +43,87 @@ if(isset($_GET["region"],$_GET["continent"])&& $_GET["continent"]!=""){
 <h1>Les pays du mondes</h1>
 
 <form action="http://localhost/Sakila/" method="GET">
-    <select name="continent" id="continent" value="<?=$continentID?>" onchange="this.form.submit()">
-    <option value="">Choisir un Continent></option>
-    <?php foreach ($continents as $continent) : ?>
-        <?php if(isset($_GET["continent"]) && $_GET["continent"]==$continent["id_continent"]) :?>
-            <option value=<?= $continent["id_continent"]?> selected><?= $continent["libelle_continent"]?></option>
-        <?php else:?>
-            <option value=<?= $continent["id_continent"]?>><?= $continent["libelle_continent"]?></option>
-        <?php endif ?>
-    <?php endforeach ?>
-    </select>  
+    <select name="continent" id="continent" value="<?= $continentID ?>" onchange="this.form.submit()">
+        <option value="">Choisir un Continent></option>
+        <?php foreach ($continents as $continent) : ?>
+            <?php if (isset($_GET["continent"]) && $_GET["continent"] == $continent["id_continent"]) : ?>
+                <option value=<?= $continent["id_continent"] ?> selected><?= $continent["libelle_continent"] ?></option>
+            <?php else : ?>
+                <option value=<?= $continent["id_continent"] ?>><?= $continent["libelle_continent"] ?></option>
+            <?php endif ?>
+        <?php endforeach ?>
+    </select>
     <select name="region" id="region" onchange="this.form.submit()">
-    <option value="">Choisir une region></option>
-    <?php foreach ($regions as $region) : ?>
-        <?php if(isset($_GET["region"]) && $_GET["region"]==$region["id_region"]) :?>
-            <option value=<?= $region["id_region"]?> selected><?= $region["libelle_region"]?></option>
-        <?php else:?>
-            <option value=<?= $region["id_region"]?>><?= $region["libelle_region"]?></option>
-        <?php endif ?>
-    <?php endforeach ?>
+        <option value="">Choisir une region></option>
+        <?php foreach ($regions as $region) : ?>
+            <?php if (isset($_GET["region"]) && $_GET["region"] == $region["id_region"]) : ?>
+                <option value=<?= $region["id_region"] ?> selected><?= $region["libelle_region"] ?></option>
+            <?php else : ?>
+                <option value=<?= $region["id_region"] ?>><?= $region["libelle_region"] ?></option>
+            <?php endif ?>
+        <?php endforeach ?>
     </select>
 </form>
 
-<section>
-    <div class="item-filter">
-        Show
-        <select name="item-number" id="item-number">
-            <option value="10">10</option>
-            <option value="25">25</option>
-            <option value="50">50</option>
-            <option value="100">100</option>
-        </select>
-    </div>
-    <div class="search">
-        <label for="search">Search:</label>
-        <input type="text" name="search" id="search">
-    </div>
-</section>
+<table id="example" class="display" style="width:100%">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($stats as $pays) : ?>
+            <tr>
+                <td><?= $pays['libelle_pays'] ?> </td>
+                <td><?= $pays['population_pays'] ?></td>
+                <td><?= $pays['taux_natalite_pays'] ?></td>
+                <td><?= $pays['taux_mortalite_pays'] ?></td>
+                <td><?= $pays['esperance_vie_pays'] ?></td>
+                <td><?= $pays['taux_mortalite_infantile_pays'] ?></td>
+            </tr>
+        <?php endforeach ?>
+            <tr>
+                <td>Donna Snider</td>
+                <td>Customer Support</td>
+                <td>New York</td>
+                <td>27</td>
+                <td>2011-01-25</td>
+                <td>$112,000</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+        </tfoot>
+    </table>
 
-<table class="table">
+<!-- <table id="example" class="display" style="width:100%">
     <thead>
         <tr>
-            <th scope="col">Pays</th>
-            <th scope="col">Population totale (en milliers)</th>
-            <th scope="col">Taux de natalité</th>
-            <th scope="col">Taux de mortalité</th>
-            <th scope="col">Espérance de vie</th>
-            <th scope="col">Taux de mortalité infantile</th>
-            <th scope="col">Nombre d'enfant(s) par femme</th>
-            <th scope="col">Taux de croissance</th>
-            <th scope="col">Population de 65 ans et plus (en milliers)</th>
+            <th>Pays</th>
+            <th>Population totale (en milliers)</th>
+            <th>Taux de natalité</th>
+            <th>Taux de mortalité</th>
+            <th>Espérance de vie</th>
+            <th>Taux de mortalité infantile</th>
+            <th>Nombre d'enfant(s) par femme</th>
+            <th>Taux de croissance</th>
+            <th>Population de 65 ans et plus (en milliers)</th>
         </tr>
     </thead>
     <tbody>
-        <?php if(isset($stats)):?>
+        <?php if (isset($stats)) : ?>
             <?php foreach ($stats as $pays) : ?>
                 <tr>
                     <td><?= $pays['libelle_pays'] ?></td>
@@ -109,15 +136,24 @@ if(isset($_GET["region"],$_GET["continent"])&& $_GET["continent"]!=""){
                     <td><?= $pays['taux_croissance_pays'] ?></td>
                     <td><?= $pays['population_plus_65_pays'] ?></td>
                 <tr>
-            <?php endforeach ?>
-        <?php else:?>
-            <tr><td colspan="15">No data available in table</td></tr>
-        <?php endif ?>
-    </tbody>  
-</table>
-<div>
-    <?=count($stats)?> Pays
-</div>
-<script>
-    
-</script>
+                <?php endforeach ?>
+            <?php else : ?>
+            <?php endif ?>
+    </tbody>
+    <tfoot>
+        <tr>
+            <th>Pays</th>
+            <th>Population totale (en milliers)</th>
+            <th>Taux de natalité</th>
+            <th>Taux de mortalité</th>
+            <th>Espérance de vie</th>
+            <th>Taux de mortalité infantile</th>
+            <th>Nombre d'enfant(s) par femme</th>
+            <th>Taux de croissance</th>
+            <th>Population de 65 ans et plus (en milliers)</th>
+        </tr>
+    </tfoot>
+</table> -->
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script src="script.js"></script>
